e996fbcedc1cbd18cef0d4702ca9724b30a20dd0
“vannyakh” <vannakkm33@gmail.com>
2026-02-23 07:35:21 +0700

    chore: remove all project files and directories to fully clean up the repository

--- app/ui/helpers/__init__.py
+++
@@ -1,5 +0,0 @@
-"""UI helpers: downloader format list, host icons."""
-
-from .downloader import DOWNLOAD_FORMATS, host_icon
-
-__all__ = ["DOWNLOAD_FORMATS", "host_icon"]
--- app/ui/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""UI utilities: formatting, text cleanup."""
-
-from .format import format_size, strip_ansi
-
-__all__ = ["format_size", "strip_ansi"]
