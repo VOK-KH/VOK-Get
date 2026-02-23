297c9defbb2452eb6b6425d6e7dc5ed44bbce8e9
“vannyakh” <vannakkm33@gmail.com>
2026-02-23 07:35:28 +0700

    chore: further clean up repository by removing additional unused files and directories

---
+++ app/ui/components/__init__.py
@@ -0,0 +1,9 @@
+"""Reusable UI components. Path: app.ui.components."""
+
+__version__ = "0.1.0"
+
+from .card_header import CardHeader
+from .download_table_card import DownloadTableCard
+from .status_table import StatusTable
+
+__all__ = ["CardHeader", "DownloadTableCard", "StatusTable"]
